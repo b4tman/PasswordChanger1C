61accc18eb894595c3668fdce5f218ebe73cdd13
Dmitry <b4tm4n@mail.ru>
2021-08-05 17:08:48 +0300

    add tests

--- Tests/PasswordChanger1C_Tests/AccessFunctions_Tests.cs
+++ Tests/PasswordChanger1C_Tests/AccessFunctions_Tests.cs
@@ -2,15 +2,30 @@
 using PasswordChanger1C;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 
 namespace PasswordChanger1C.Tests
 {
     public class AccessFunctions_Tests
     {
-        [Fact()]
-        public void ReadInfoBase_Test()
+        const string File838 = "./Resources/8.3.8.1CD";
+        const string File8214 = "./Resources/8.2.14.1CD";
+
+        [Theory]
+        [InlineData(File838)]
+        [InlineData(File8214)]
+        public void Resources_Test(string filename)
         {
-            Assert.True(false, "This test needs an implementation");
+            Assert.True(File.Exists(filename));
+        }
+
+        [Theory]
+        [InlineData(File838, "8.3.8")]
+        [InlineData(File8214, "8.2.14")]
+        public void ReadInfoBase_CheckVersions(string filename, string version)
+        {
+            var TableParams = AccessFunctions.ReadInfoBase(filename, "V8USERS");
+            Assert.Equal(TableParams.DatabaseVersion, version);
         }
     }
 }
