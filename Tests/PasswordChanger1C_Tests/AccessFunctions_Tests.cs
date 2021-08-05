375c2696a879abb9a9041adde6251eff923d4a83
Dmitry <b4tm4n@mail.ru>
2021-08-05 16:35:34 +0300

    add testing project

---
+++ Tests/PasswordChanger1C_Tests/AccessFunctions_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using PasswordChanger1C;
+using Newtonsoft.Json;
+using System;
+
+namespace PasswordChanger1C.Tests
+{
+    public class AccessFunctions_Tests
+    {
+        [Fact()]
+        public void ReadInfoBase_Test()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
