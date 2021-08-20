3f40c3bf9c9949cbac3788abd2423d91f98eb247
Dmitry <b4tm4n@mail.ru>
2021-08-20 09:37:40 +0300

    move InfobaseBinaryReader

--- PasswordChanger1C/AccessFunctions.cs
+++ PasswordChanger1C/AccessFunctions.cs
@@ -27,7 +27,7 @@ namespace PasswordChanger1C
     }
 
     
-    public static class AccessFunctions
+    public static partial class AccessFunctions
     {
         private const string InfobaseFile_Sign = "1CDBMSV8";
         public const string InfoBaseRepo_EmptyPassword = "d41d8cd98f00b204e9800998ecf8427e";
@@ -70,77 +70,6 @@ namespace PasswordChanger1C
             public string TableDefinition;
             public string DatabaseVersion;
             public byte[] BinaryData;
-        }
-
-        public class InfobaseBinaryReader: BinaryReader
-        {
-            /// <summary>
-            ///  Error when Read returned not expected size of bytes
-            /// </summary>
-            public class PageReadException : IOException
-            {
-                public PageReadException(string message) : base(message)
-                {
-                }
-                public PageReadException(int PageSize, int ReadedSize) : base($"Read Page with size {PageSize} returned {ReadedSize} bytes")
-                {
-                }
-            }
-
-            /// <summary>
-            ///  Error when PageSize not set
-            /// </summary>
-            public class PageSizeNotSetException : IOException
-            {
-                public PageSizeNotSetException(string message) : base(message)
-                {
-                }
-                public PageSizeNotSetException() : this("PageSize not set")
-                {
-                }
-            }
-
-            public int PageSize { get; set; }
-
-            public InfobaseBinaryReader(Stream input): base(input)
-            {
-                    
-            }
-
-            public InfobaseBinaryReader(Stream input, int _PageSize) : base(input)
-            {
-                PageSize = _PageSize;
-            }
-
-            public long Seek(long offset, SeekOrigin origin)
-            {
-                return BaseStream.Seek(offset, origin);
-            }
-
-            public void SeekToPage(long PageNumber)
-            {
-                Seek(PageNumber * PageSize, SeekOrigin.Begin);
-            }
-
-            public override byte[] ReadBytes(int count)
-            {
-                var result = base.ReadBytes(count);
-                if (result.Length != count) throw new PageReadException(result.Length, count);
-                return result;
-            }
-
-            public byte[] ReadPage()
-            {
-                if (PageSize == 0) throw new PageSizeNotSetException();
-                return ReadBytes(PageSize);
-            }
-
-            public byte[] ReadPage(long PageNumber)
-            {
-                SeekToPage(PageNumber);
-                return ReadPage();
-            }
-
         }
 
         public static PageParams ReadInfoBase(in string FileName, in string TargetTableName)
