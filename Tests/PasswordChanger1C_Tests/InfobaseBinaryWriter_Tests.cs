abc619ec48f21225e58c24e3152f03a787b5787b
Dmitry <b4tm4n@mail.ru>
2021-09-01 12:58:49 +0300

    add WritePages tests

--- Tests/PasswordChanger1C_Tests/InfobaseBinaryWriter_Tests.cs
+++ Tests/PasswordChanger1C_Tests/InfobaseBinaryWriter_Tests.cs
@@ -1,8 +1,8 @@
-﻿using Xunit;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Xunit;
 
 namespace PasswordChanger1C.Tests
 {
@@ -26,12 +26,84 @@ namespace PasswordChanger1C.Tests
         }
 
         [Fact()]
+        public void WritePages_Seq()
+        {
+            string Output;
+            const int PageSize = 10;
+            const int PagesCount = 10;
+            const int PatchSize = PageSize * 3;
+            long[] changePages = { 3, 4, 5 };
+
+            string BlankPage = new String('-', PageSize);
+            string Blank = string.Concat(Enumerable.Repeat(BlankPage, PagesCount));
+            string Patch = new String('+', PatchSize);
+            var Blank_bin = System.Text.Encoding.ASCII.GetBytes(Blank);
+            var Patch_bin = System.Text.Encoding.ASCII.GetBytes(Patch);
+
+            using (var test_stream = new MemoryStream(Blank_bin))
+            {
+                using var writer = new InfobaseBinaryWriter(test_stream, PageSize);
+                writer.WritePages(changePages.ToList(), Patch_bin);
+                Output = streamToString(test_stream);
+            }
+
+            Assert.Collection(SplitByLength(Output, PageSize),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page)
+            );
+        }
+
+        [Fact()]
+        public void WritePages_SkipPage()
+        {
+            string Output;
+            const int PageSize = 10;
+            const int PagesCount = 10;
+            const int PatchSize = PageSize * 3;
+            long[] changePages = { 3, 5, 7 };
+
+            string BlankPage = new String('-', PageSize);
+            string Blank = string.Concat(Enumerable.Repeat(BlankPage, PagesCount));
+            string Patch = new String('+', PatchSize);
+            var Blank_bin = System.Text.Encoding.ASCII.GetBytes(Blank);
+            var Patch_bin = System.Text.Encoding.ASCII.GetBytes(Patch);
+
+            using (var test_stream = new MemoryStream(Blank_bin))
+            {
+                using var writer = new InfobaseBinaryWriter(test_stream, PageSize);
+                writer.WritePages(changePages.ToList(), Patch_bin);
+                Output = streamToString(test_stream);
+            }
+
+            Assert.Collection(SplitByLength(Output, PageSize),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal("++++++++++", Page),
+                (Page) => Assert.Equal(BlankPage, Page),
+                (Page) => Assert.Equal(BlankPage, Page)
+            );
+        }
+
+        [Fact()]
         public void WriteToPagesAt_Seq()
         {
             string Output;
             const int PageSize = 10;
             const int PagesCount = 10;
-            const int offset = PageSize + PageSize/2;
+            const int offset = PageSize + PageSize / 2;
             const int PatchSize = PageSize * 2 + 3;
             long[] changePages = { 2, 3, 4, 5 };
 
@@ -91,7 +163,7 @@ namespace PasswordChanger1C.Tests
                 (Page) => Assert.Equal(BlankPage, Page),
                 (Page) => Assert.Equal("@@@@@@@@@@", Page),
                 (Page) => Assert.Equal(BlankPage, Page),
-                (Page) => Assert.Equal("@@@@@@@@##", Page),   
+                (Page) => Assert.Equal("@@@@@@@@##", Page),
                 (Page) => Assert.Equal(BlankPage, Page),
                 (Page) => Assert.Equal(BlankPage, Page)
             );
